07b90da12b033a388c5e4934db625c886e3fd9f0
jawad <jawad@correctcloud.io>
2025-09-29 11:54:23 +0530

    Update database configuration and enhance Product model

--- src/main/java/com/apis/product/Product.java
+++ src/main/java/com/apis/product/Product.java
@@ -1,7 +1,9 @@
 package com.apis.product;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
@@ -9,6 +11,8 @@ import org.springframework.data.relational.core.mapping.Table;
 @Data
 @Table("products")
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
 
     @Id
